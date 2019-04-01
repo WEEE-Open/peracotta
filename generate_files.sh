8130958b4cc274abfc8ebe6df411dd0a0e290cad
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-04-01 19:25:25 +0200

    .sh file, closes #10

--- generate_files.sh
+++ generate_files.sh
@@ -3,4 +3,9 @@
 dmidecode -t baseboard >> baseboard.txt
 dmidecode -t chassis >> chassis.txt
 lscpu >> lscpu.txt
-# add other commands
+lspci -v >> lspci.txt
+smartctl -x /dev/sda >> smartctl.txt
+# Already done on our custom distro, but repetita iuvant
+modprobe at24
+modprobe eeprom
+decode-dimms >> dimms.txt
