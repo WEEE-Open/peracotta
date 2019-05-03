2b17010f8c2d346dcd732da79ed2e71736425229
e-caste-mbp <en.cas.dev@icloud.com>
2019-05-03 17:21:43 +0200

    added compatibility for optional path for cleaner output format

--- generate_files.sh
+++ generate_files.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
-dmidecode -t baseboard > $1/baseboard.txt
-dmidecode -t connector > $1/connector.txt
-dmidecode -t chassis > $1/chassis.txt
-lscpu > $1/lscpu.txt
-lspci -v > $1/lspci.txt
-glxinfo > $1/glxinfo.txt
+if [ $# -eq 0 ]; then
+    echo "No path given: outputting files in working directory"
+    OUTPATH="."
+elif [ $# -eq 1 ]; then
+    echo "Outputting files to "$1
+    OUTPATH=$1
+else
+    echo "Unexpected number of parameters. Usage: sudo ./generate_files.sh /optional/path/to/files"
+fi
+
+dmidecode -t baseboard > $OUTPATH/baseboard.txt
+dmidecode -t connector > $OUTPATH/connector.txt
+dmidecode -t chassis > $OUTPATH/chassis.txt
+lscpu > $OUTPATH/lscpu.txt
+lspci -v > $OUTPATH/lspci.txt
+glxinfo > $OUTPATH/glxinfo.txt
 DISKZ=($(lsblk -d -I 8 -o NAME -n))
 echo Found ${#DISKZ[@]} disks
 for d in "${DISKZ[@]}"
 do
-	smartctl -x /dev/$d > $1/smartctl-dev-$d.txt
+	smartctl -x /dev/$d > $OUTPATH/smartctl-dev-$d.txt
 done
 # Already done on our custom distro, but repetita iuvant
 modprobe at24
 modprobe eeprom
-decode-dimms > $1/dimms.txt
+decode-dimms > $OUTPATH/dimms.txt
