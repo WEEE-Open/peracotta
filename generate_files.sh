3b77be2f22caeefee1ba561451ba89e0080e2b15
e-caste-mbp <en.cas.dev@icloud.com>
2019-05-03 17:11:07 +0200

    added  to output files to first argument

--- generate_files.sh
+++ generate_files.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-dmidecode -t baseboard > baseboard.txt
-dmidecode -t connector > connector.txt
-dmidecode -t chassis > chassis.txt
-lscpu > lscpu.txt
-lspci -v > lspci.txt
-glxinfo > glxinfo.txt
+dmidecode -t baseboard > $1/baseboard.txt
+dmidecode -t connector > $1/connector.txt
+dmidecode -t chassis > $1/chassis.txt
+lscpu > $1/lscpu.txt
+lspci -v > $1/lspci.txt
+glxinfo > $1/glxinfo.txt
 DISKZ=($(lsblk -d -I 8 -o NAME -n))
 echo Found ${#DISKZ[@]} disks
 for d in "${DISKZ[@]}"
 do
-	smartctl -x /dev/$d > smartctl-dev-$d.txt
+	smartctl -x /dev/$d > $1/smartctl-dev-$d.txt
 done
 # Already done on our custom distro, but repetita iuvant
 modprobe at24
 modprobe eeprom
-decode-dimms > dimms.txt
+decode-dimms > $1/dimms.txt
