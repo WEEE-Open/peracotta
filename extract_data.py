efd78668e741192dc90ca4e0650c8cbcdbdef7d0
e-caste-mbp <en.cas.dev@icloud.com>
2019-05-09 22:59:08 +0200

    first draft of how the list of dicts should be created

--- extract_data.py
+++ extract_data.py
@@ -10,11 +10,75 @@ from read_lspci_and_glxinfo import read_lspci_and_glxinfo
 
 
 def extract_and_collect_data_from_generated_files(has_dedicated_gpu: bool):
-    return [
-            read_lspci_and_glxinfo(has_dedicated_gpu, "tmp/lspci.txt", "tmp/glxinfo.txt"),
-            read_decode_dimms("tmp/dimms.txt")
-    ]
+
+    # TODO: refactor after testing
+    # directory = "tmp"
+    directory = "tests/dismone"
+
+    dimms = read_decode_dimms(directory + "/dimms.txt")
+    lspci_glxinfo = read_lspci_and_glxinfo(has_dedicated_gpu, directory + "/lspci.txt", directory + "/glxinfo.txt")
+
+    no_dimms_str = "decode-dimms was not able to find any RAM details"
+
+    # the None check MUST come before the others
+    if dimms is None or no_dimms_str in dimms:
+        # empty default dictionary
+        dimms = {
+            "type": "ram",
+            "brand": None,
+            "model": None,
+            "serial_number": None,
+            "frequency": None,
+            "human_readable_frequency": None,
+            "capacity": None,
+            "human_readable_capacity": None,
+            "RAM_type": None,
+            "ECC": None
+            # "CAS_latencies": None # feature missing from TARALLO
+        }
+
+    no_gpu_info_str = "I couldn't find the Video Card brand. The model was set to 'None' and is to be edited logging into " \
+                  "the TARALLO afterwards. The information you're looking for should be in the following 2 lines:"
+    no_vram_info_str = "A dedicated video memory couldn't be found. A generic video memory capacity was found instead, which " \
+                  "could be near the actual value. Please humans, fix this error by hand."
+
+    if lspci_glxinfo is None or (no_gpu_info_str in lspci_glxinfo and no_vram_info_str in lspci_glxinfo):
+        lspci_glxinfo = {
+            "type": "graphics-card",
+            "manufacturer_brand": None,
+            "reseller_brand": None,
+            "model": None,
+            "capacity": None,
+            "human_readable_capacity": None
+        }
+        print_lspci_lines_in_dialog = True
+
+    elif no_vram_info_str in lspci_glxinfo and not no_gpu_info_str in lspci_glxinfo:
+        # TODO: check output in this case, change only VRAM field to None
+        print_lspci_lines_in_dialog = False
+
+    else:
+        print_lspci_lines_in_dialog = False
+
+
+    result = []
+
+    if isinstance(dimms, dict):
+        # otherwise it will append every key-value pair of the dict
+        result.append(dimms)
+    else:
+        for dimm in dimms:
+            result.append(dimm)
+
+    # assuming there is only 1 graphics card in the system
+    result.append(lspci_glxinfo)
+
+    # tuple = list(dicts), bool
+    return result, print_lspci_lines_in_dialog
 
 
 if __name__ == '__main__':
-    extract_and_collect_data_from_generated_files()
+    # TODO: refactor after testing
+    # extract_and_collect_data_from_generated_files()
+    sys_info = extract_and_collect_data_from_generated_files(True)
+    print(sys_info)
