389472bcdd851a74803221737b6764671c3f3b70
e-caste-mbp <en.cas.dev@icloud.com>
2019-05-10 13:17:42 +0200

    mega refactor, add class, standalone mode, list(dict) return and TARALLO compatible dict key names

--- read_smartctl.py
+++ read_smartctl.py
@@ -1,60 +1,112 @@
 #!/usr/bin/env python3
-import sys
+import sys, os
 
-if sys.argv.__len__() < 1:
-    print("Invalid call, specify filename to read")
-    exit(0)
+# if sys.argv.__len__() < 1:
+#     print("Invalid call, specify filename to read")
+#     exit(0)
 
-filepath = sys.argv[1]
 
 '''
 Read "smartctl" output:
 '''
 
-f = open(filepath, 'r')
+class Disk():
+    def __init__(self):
+        self.type = ""
+        self.brand = ""
+        self.model = ""
+        self.wwn = ""
+        self.serial_number = ""
+        self.capacity = -1  # n of bytes
+        self.human_readable_capacity = ""
+        self.rotation_rate = -1
+        self.smart_data = False
 
-if f == 0:
-    print("cannot open file")
-    print("Make sure to execute 'sudo ./generate_files.sh' first!")
-    exit(0)
+# THE PATH HERE ONLY POINTS TO THE DIRECTORY, eg. tmp, AND NOT TO THE FILE, e.g. tmp/smartctl-dev-sda.txt,
+# SINCE THERE MAY BE MULTIPLE FILES
+def read_smartctl(path: str):
+    try:
+        disks = []
+
+        for filename in os.listdir(path):
+            if "smartctl-dev-" in filename:
+
+                disk = Disk()
+
+                with open(path + "/" + filename, 'r') as f:
+                    output = f.read()
+
+                for line in output.splitlines():
+                    if "Model Family" in line:
+                        disk.brand = line.split("Model Family:")[1].strip()
+
+                    elif "Device Model" in line:
+                        disk.model = line.split("Device Model:")[1].strip()
+
+                    elif "Serial Number" in line:
+                        disk.serial_number = line.split("Serial Number:")[1].strip()
+
+                    elif "LU WWN Device Id" in line:
+                        disk.wwn = line.split("LU WWN Device Id:")[1].strip()
+
+                    elif "User Capacity" in line:
+                        tmp_capacity = line.split("[")[1].split("]")[0]
+                        if tmp_capacity is not None:
+                            disk.human_readable_capacity = tmp_capacity
+                            if "GB" in tmp_capacity:
+                                disk.capacity = int(tmp_capacity[:-2]) * 1024 * 1024 * 1024
+
+                            elif "MB" in tmp_capacity:
+                                disk.capacity = int(tmp_capacity[:-2]) * 1024 * 1024
+
+                            elif "KB" in tmp_capacity.upper():
+                                disk.capacity = int(tmp_capacity[:-2]) * 1024
+
+                    elif "Rotation Rate" in line:
+                        if "Solid State Device" not in line:
+                            disk.rotation_rate = int(line.split("Rotation Rate:")[1].split("rpm")[0].strip())
+                            disk.type = "hdd"
+                        else:
+                            disk.type = "ssd"
+
+                    elif "SMART support is" in line:
+                        if "Available" in line or "Enabled" in line:
+                            disk.smart_data = True
+
+                disks.append(disk)
+
+        result = []
+        for disk in disks:
+            if disk.type == "hdd":
+                result.append({
+                    "type": "hdd",
+                    "brand": disk.brand,
+                    "model": disk.model,
+                    "wwn": disk.wwn,
+                    "sn": disk.serial_number,
+                    "capacity-decibyte": int(disk.capacity / 10),
+                    "human_readable_capacity": disk.human_readable_capacity,
+                    "spin-rate-rpm": disk.rotation_rate,
+                    "smart-data": disk.smart_data
+                })
+            else: #ssd
+                result.append({
+                    "type": "ssd",
+                    "brand": disk.brand,
+                    "model": disk.model,
+                    "sn": disk.serial_number,
+                    "capacity-byte": disk.capacity,
+                    "human_readable_capacity": disk.human_readable_capacity,
+                    "smart-data": disk.smart_data
+                })
+
+        return result
+
+    except FileNotFoundError:
+        print("Cannot open file.")
+        print("Make sure to execute 'sudo ./generate_files.sh' first!")
+        exit(-1)
 
 
-brand = ""
-model = ""
-wwn = ""
-serial_number = ""
-capacity = 0  # n of bytes
-rotation_rate = ""
-
-output = f.read()
-# print(output)
-
-strings = output.splitlines()
-
-for sub in strings:
-    if sub.startswith("Model Family:"):
-        brand = sub[18:]
-
-    if sub.startswith("Device Model:"):
-        model = sub[18:]
-
-    if sub.startswith("Serial Number:"):
-        serial_number = sub[18:]
-
-    if sub.startswith("LU WWN Device Id:"):
-        wwn = sub[20:26]
-
-    if sub.startswith("User Capacity:"):
-        capacity = sub[18:]
-        capacity_list = capacity.split(" ")
-        capacity = capacity_list[0] + " " + capacity_list[1]
-
-    if sub.startswith("Rotation Rate:"):
-        rotation_rate = sub[18:]
-
-
-smartctl_list = {'Brand': brand, 'Model': model, 'Serial Number': serial_number, 'WWN': wwn, 'Capacity': capacity, 'Rotation_rate': rotation_rate}
-print("baseboard_list: ", smartctl_list)
-
-print()
-f.close()
+if __name__ == '__main__':
+    print(read_smartctl(sys.argv[1]))
