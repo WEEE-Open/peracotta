1d02742e14fe2b852ff905ac631e7fae7e7df847
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-07-01 13:19:58 +0200

    Split stuff, add connectors stuff

--- tests/test_77.py
+++ tests/test_77.py
@@ -2,14 +2,13 @@
 
 import pytest
 # import smartctl
-from read_decode_dimms import read_decode_dimms
-# import read_dmidecode_and_lscpu
+from read_dmidecode import get_baseboard, get_chassis
 from read_lspci_and_glxinfo import read_lspci_and_glxinfo
+
+filedir = '77/'
 
 
 def test_77_lspci():
-	filedir = '77/'
-
 	expect = {
 		'type': 'graphics-card',
 		'brand-manufacturer': 'SiS',
@@ -20,4 +19,29 @@ def test_77_lspci():
 	output = read_lspci_and_glxinfo(False, filedir + 'lspci.txt', filedir + 'glxinfo.txt')
 
 	assert output == expect
+
+
+def test_77_baseboard():
+	expect = {
+		'brand': 'ASUSTeK Computer INC.',
+		'model': 'P5SD2-VM',
+		'sn': 'MT721CT11114269',
+		'type': 'motherboard'
+	}
+	output = get_baseboard(filedir + 'baseboard.txt')
+
+	assert output == expect
+
+
+def test_77_chassis():
+	expect = {
+		'brand': 'ASUSTeK Computer INC.',
+		'model': '',
+		'sn': 'MT721CT11114269',
+		'type': 'case',
+	}
+	output = get_chassis(filedir + 'baseboard.txt')
+
+	assert output == expect
+
 # TODO: more tests
