7a1234b1c475c7ed6f73715990563ead977a7f6d
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-07-02 12:01:28 +0200

    More tests, more connectors, more wrong results

--- tests/test_asdpc.py
+++ tests/test_asdpc.py
@@ -3,13 +3,13 @@
 import pytest
 # import smartctl
 from read_decode_dimms import read_decode_dimms
-# import read_dmidecode_and_lscpu
+from read_dmidecode import get_baseboard, get_chassis, get_connectors
 from read_lspci_and_glxinfo import read_lspci_and_glxinfo
 
+filedir = 'asdpc/'
 
-def test_asdpc_lspci():
-	filedir = 'asdpc/'
 
+def test_lspci():
 	expect = {
 		'type': 'graphics-card',
 		'brand-manufacturer': 'AMD/ATI',
@@ -23,9 +23,7 @@ def test_asdpc_lspci():
 	assert output == expect
 
 
-def test_asdpc_ram():
-	filedir = 'asdpc/'
-
+def test_ram():
 	expect = [
 		{
 			'ram-ecc': 'no',
@@ -36,9 +34,9 @@ def test_asdpc_ram():
 			'human_readable_capacity': '8192 MB',
 			'human_readable_frequency': '1333 MHz',
 			'model': 'F3-1600C7-8GTX',
-			'serial_number': '',
+			'sn': '',
 			'type': 'ram',
-			'ram-timings': '10T, 9T, 8T, 7T, 6T, 5T',
+			'ram-timings': '9-9-9-24',
 		},
 		{
 			'ram-ecc': 'no',
@@ -49,14 +47,76 @@ def test_asdpc_ram():
 			'human_readable_capacity': '8192 MB',
 			'human_readable_frequency': '1333 MHz',
 			'model': 'F3-1600C7-8GTX',
-			'serial_number': '',
+			'sn': '',
 			'type': 'ram',
-			'ram-timings': '10T, 9T, 8T, 7T, 6T, 5T',
+			'ram-timings': '9-9-9-24',
 		}
 	]
 	output = read_decode_dimms(filedir + 'dimms.txt')
 
 	assert len(output) == 2, "2 RAM modules are found"
+	assert output == expect
+
+
+def test_baseboard():
+	expect = {
+		'brand': 'Gigabyte Technology Co., Ltd.',
+		'model': '970A-DS3P FX',
+		'sn': 'To be filled by O.E.M.',
+		'type': 'motherboard'
+	}
+	output = get_baseboard(filedir + 'baseboard.txt')
+
+	assert output == expect
+
+
+def test_connector():
+	baseboard = get_baseboard(filedir + 'baseboard.txt')
+
+	# This is entirely wrong and is not reflected by any means from reality and the real motherboard, but the manufacturer
+	# dropped all this garbage into the DMI information, so here we go...
+	expect = {
+		'brand': 'Gigabyte Technology Co., Ltd.',
+		'model': '970A-DS3P FX',
+		'sn': 'To be filled by O.E.M.',
+		'type': 'motherboard',
+		'usb-ports-n': 3,
+		'ps2-ports-n': 2,
+		'serial-ports-n': 1,
+		'warning': 'Unknown connector: None / Mini Centronics Type-14\n'
+		'Unknown connector: None / DB-15 female\n'
+		'Unknown connector: Other / None (J9A1 - TPM HDR / Not Specified)\n'
+		'Unknown connector: Other / None (J9C1 - PCIE DOCKING CONN / Not Specified)\n'
+		'Unknown connector: Other / None (J2B3 - CPU FAN / Not Specified)\n'
+		'Unknown connector: Other / None (J6C2 - EXT HDMI / Not Specified)\n'
+		'Unknown connector: Other / None (J3C1 - GMCH FAN / Not Specified)\n'
+		'Unknown connector: Other / None (J1D1 - ITP / Not Specified)\n'
+		'Unknown connector: Other / None (J9E2 - MDC INTPSR / Not Specified)\n'
+		'Unknown connector: Other / None (J9E4 - MDC INTPSR / Not Specified)\n'
+		'Unknown connector: Other / None (J9E3 - LPC HOT DOCKING / Not Specified)\n'
+		'Unknown connector: Other / None (J9E1 - SCAN MATRIX / Not Specified)\n'
+		'Unknown connector: Other / None (J9G1 - LPC SIDE BAND / Not Specified)\n'
+		'Unknown connector: Other / None (J8F1 - UNIFIED / Not Specified)\n'
+		'Unknown connector: Other / None (J6F1 - LVDS / Not Specified)\n'
+		'Unknown connector: Other / None (J2F1 - LAI FAN / Not Specified)\n'
+		'Unknown connector: Other / None (J2G1 - GFX VID / Not Specified)\n'
+		'Unknown connector: Other / None (J1G6 - AC JACK / Not Specified)',
+	}
+	output = get_connectors(filedir + 'connector.txt', baseboard)
+
+	assert output == expect
+
+
+def test_chassis():
+	# This is also wrong, but for pre-assembled computers it should be right
+	expect = {
+		'brand': 'Gigabyte Technology Co., Ltd.',
+		'model': '',
+		'sn': 'To be filled by O.E.M.',
+		'type': 'case',
+	}
+	output = get_chassis(filedir + 'baseboard.txt')
+
 	assert output == expect
 
 # TODO: more tests
