f81035c6f689f37c7677fbc5d603997fddaa4da6
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-07-06 11:33:13 +0200

    net.txt

--- generate_files.sh
+++ generate_files.sh
@@ -10,18 +10,29 @@ else
     echo "Unexpected number of parameters. Usage: sudo ./generate_files.sh /optional/path/to/files"
 fi
 
-dmidecode -t baseboard > $OUTPATH/baseboard.txt
-dmidecode -t connector > $OUTPATH/connector.txt
-dmidecode -t chassis > $OUTPATH/chassis.txt
-ip link > $OUTPATH/iplink.txt
-lscpu > $OUTPATH/lscpu.txt
-lspci -v > $OUTPATH/lspci.txt
-glxinfo > $OUTPATH/glxinfo.txt
+dmidecode -t baseboard > "$OUTPATH/baseboard.txt"
+dmidecode -t connector > "$OUTPATH/connector.txt"
+dmidecode -t chassis > "$OUTPATH/chassis.txt"
+truncate "$OUTPATH/net.txt"
+NET=($(find /sys/class/net -maxdepth 1 \( -name "en*" -o -name "wl*" \) -exec basename  '{}' ';'))
+for NETDEV in "${NET[@]}"
+do
+	ADDRESS=$(cat /sys/class/net/$NETDEV/address)
+	if [[ $? -ne 0 ]]
+	then
+		echo The \"invalid argument\" error above is normal, disregard it
+	fi
+	SPEED=$(cat /sys/class/net/$NETDEV/speed)
+	echo $NETDEV $ADDRESS $SPEED >> "$OUTPATH/net.txt"
+done
+lscpu > "$OUTPATH/lscpu.txt"
+lspci -v > "$OUTPATH/lspci.txt"
+glxinfo > "$OUTPATH/glxinfo.txt"
 DISKZ=($(lsblk -d -I 8 -o NAME -n))
 echo Found ${#DISKZ[@]} disks
 for d in "${DISKZ[@]}"
 do
-	smartctl -x /dev/$d > $OUTPATH/smartctl-dev-$d.txt
+	smartctl -x /dev/$d > "$OUTPATH/smartctl-dev-$d.txt"
 done
 # Already done on our custom distro, but repetita iuvant
 modprobe at24
