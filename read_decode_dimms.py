2be0cf38180606cce8322caa850d2aa1618016d3
e-caste <48513706+e-caste@users.noreply.github.com>
2019-11-29 16:35:21 +0100

    2 lines of space are enough

--- read_decode_dimms.py
+++ read_decode_dimms.py
@@ -29,7 +29,6 @@ class ECC(Enum):
 	not_available = "no"
 
 
-
 # initial_chars_to_ignore is the length of the feature whose name the line begins with
 # e.g. "Fundamental Memory Type" begins with 23 characters that are not all spaces, then n spaces to ignore,
 # and finally there's the value needed, e.g. "DDR3 SDRAM"
