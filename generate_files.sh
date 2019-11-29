f8ae0ac328f757f0156c9ba511328cffd6d89e82
e-caste <48513706+e-caste@users.noreply.github.com>
2019-11-29 16:28:53 +0100

    remove stderr from generated files

--- generate_files.sh
+++ generate_files.sh
@@ -13,9 +13,9 @@ else
     echo -n "Unexpected number of parameters.\nUsage: sudo ./generate_files.sh /optional/path/to/files"
 fi
 
-dmidecode -t baseboard &> "$OUTPATH/baseboard.txt"
-dmidecode -t connector &> "$OUTPATH/connector.txt"
-dmidecode -t chassis &> "$OUTPATH/chassis.txt"
+dmidecode -t baseboard > "$OUTPATH/baseboard.txt"
+dmidecode -t connector > "$OUTPATH/connector.txt"
+dmidecode -t chassis > "$OUTPATH/chassis.txt"
 truncate -s 0 "$OUTPATH/net.txt"  # Create empty file or delete content
 NET=($(find /sys/class/net -maxdepth 1 \( -name "en*" -o -name "wl*" \) -exec basename  '{}' ';'))
 for NETDEV in "${NET[@]}"
@@ -28,15 +28,15 @@ do
 	SPEED=$(cat /sys/class/net/$NETDEV/speed)
 	echo $NETDEV $ADDRESS $SPEED >> "$OUTPATH/net.txt"
 done
-lscpu &> "$OUTPATH/lscpu.txt"
-lspci -v &> "$OUTPATH/lspci.txt"
-glxinfo &> "$OUTPATH/glxinfo.txt"
+lscpu > "$OUTPATH/lscpu.txt"
+lspci -v > "$OUTPATH/lspci.txt"
+glxinfo > "$OUTPATH/glxinfo.txt"
 DISKZ=($(lsblk -d -I 8 -o NAME -n))
 echo Found ${#DISKZ[@]} disks
 for d in "${DISKZ[@]}"; do
-	smartctl -x /dev/$d &> "$OUTPATH/smartctl-dev-$d.txt"
+	smartctl -x /dev/$d > "$OUTPATH/smartctl-dev-$d.txt"
 done
 # Already done on our custom distro, but repetita iuvant
 modprobe at24
 modprobe eeprom
-decode-dimms &> "$OUTPATH/dimms.txt"
+decode-dimms > "$OUTPATH/dimms.txt"
