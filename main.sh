7218bd7a91215be072beb2990fa297e05768579a
e-caste-mbp <en.cas.dev@icloud.com>
2019-12-04 00:03:30 +0100

    better string formatting

--- main.sh
+++ main.sh
@@ -4,13 +4,10 @@ function print_usage {
   echo "Use -h or --help to show this help."
   echo "Usage: $0 [-p|--path <optional path to generate files to>] [-c|--cpu | -g|--gpu | -b|--motherboard]"
   echo ""
-  echo "If no argument is given, then this script will interactively guide you to "
-  echo "run the PERACOTTA data gathering package."
+  echo "If no argument is given, then this script will interactively guide you to run the PERACOTTA data gathering package."
   echo ""
-  echo "Alternatively, you can choose to pass either the path to the directory where you want the "
-  echo "files to be generated, the gpu location, or both."
-  echo "In this case, the script will only become interactive when needed, and it won't ask you anything "
-  echo "if you pass both the path and the gpu location."
+  echo "Alternatively, you can choose to pass either the path to the directory where you want the files to be generated, the gpu location, or both."
+  echo "In this case, the script will only become interactive when needed, and it won't ask you anything if you pass both the path and the gpu location."
 }
 
 function check_mutually_exclusive_args {
@@ -33,9 +30,7 @@ function print_gpu_prompt {
 
 function run_extract_data {
   echo ""
-  echo "The following output can be copy-pasted "
-  echo "into the 'Bulk Add' page of the TARALLO, "
-  echo "from '[' to ']':"
+  echo "The following output can be copy-pasted into the 'Bulk Add' page of the TARALLO, from '[' to ']':"
   echo ""
   ./extract_data.py -$gpu_location "$OUTPUT_PATH"
 }
@@ -78,6 +73,7 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+# if output path is not passed as argument ask the user
 if [ -z $OUTPUT_PATH ]; then
   if [ -d tmp ]; then
     echo "Overwrite existing files in tmp dir? y/N"
