9999cb85a600fdfc82a92668a2b4318e24ce59d7
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-03 20:17:31 +0200

    add script to easily install gui dependencies

---
+++ install_dependencies_gui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./install_dependencies_all.sh
+
+sudo apt install python3-pip python3-venv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+# remember to `deactivate` to exit the venv
