8359fedd2d90be13a5d03e24dbcb62d30ae56467
e-caste-mbp <en.cas.dev@icloud.com>
2020-05-03 20:18:57 +0200

    add check for existing venv directory

--- install_dependencies_gui.sh
+++ install_dependencies_gui.sh
@@ -3,7 +3,9 @@
 ./install_dependencies_all.sh
 
 sudo apt install python3-pip python3-venv
-python3 -m venv venv
+if [ ! -d venv ]; then
+  python3 -m venv venv
+fi
 source venv/bin/activate
 pip install -r requirements.txt
 
