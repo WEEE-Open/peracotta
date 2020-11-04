62b4fb636cd70f86dd379e7414b447510f941990
e-caste-ubvmprox <en.cas.dev@icloud.com>
2020-11-04 20:06:00 +0100

    add Docker compatibility

--- install_dependencies_all.sh
+++ install_dependencies_all.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-apt install -y pciutils i2c-tools mesa-utils smartmontools dmidecode < /dev/null
+CMD="apt install -y pciutils i2c-tools mesa-utils smartmontools dmidecode < /dev/null"
+
+if [ "$EUID" -ne 0 ]; then
+  sudo "$CMD"
+else
+  /bin/bash -c "$CMD"
+fi
