569f02f00b8b39c61a0bece24de6785cf045ee07
e-caste-ubvmprox <en.cas.dev@icloud.com>
2020-11-04 13:13:13 +0100

    remove sudo for Docker compatibility. ref #58

--- install_dependencies_all.sh
+++ install_dependencies_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install -y pciutils i2c-tools mesa-utils smartmontools dmidecode < /dev/null
+apt install -y pciutils i2c-tools mesa-utils smartmontools dmidecode < /dev/null
