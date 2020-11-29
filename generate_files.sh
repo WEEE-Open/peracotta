69852b48941be7e196e94c75daf5f1d7e0de1c26
lvps <lvps@users.noreply.github.com>
2020-11-29 14:59:30 +0100

    Add mkdir to generate_files.sh
    So it's possible to use /tmp/whatever and have it created

--- generate_files.sh
+++ generate_files.sh
@@ -8,9 +8,15 @@ if [ $# -eq 0 ]; then
     OUTPATH="."
 elif [ $# -eq 1 ]; then
     echo "Outputting files to "$1
+    mkdir -p -- "$1"
+    if [ ! $# -eq 0 ]; then
+    	echo "Failed to create or access output directory"
+    	exit 1
+    fi
     OUTPATH=$1
 else
     echo -n "Unexpected number of parameters.\nUsage: sudo ./generate_files.sh /optional/path/to/files"
+    exit 1
 fi
 
 dmidecode -t baseboard > "$OUTPATH/baseboard.txt"
