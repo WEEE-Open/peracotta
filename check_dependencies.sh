a1ba69fcf3adb9ce5dbdf014e93465561401bab7
Pinzia <s285028@studenti.polito.it>
2020-12-17 14:06:03 +0100

    Changed check_dependencies.sh. Now it checks all the dependencies not just the first one

--- check_dependencies.sh
+++ check_dependencies.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if ! command -v pciutils i2c-tools mesa-utils smartmontools dmidecode >/dev/null 2>&1; then
-  exit 1
-else
+dpkg -s pciutils i2c-tools mesa-utils smartmontools dmidecode &> /dev/null
+
+if [ $? -eq 0 ]; then
   exit 0
+else
+  exit 1
 fi
