50aa3eb5a33c56bbcd6bc96ba742ab94e64ce87e
Pinzia <s285028@studenti.polito.it>
2020-12-22 11:43:16 +0100

    implemented the required changes

--- Installing.py
+++ Installing.py
@@ -1,30 +1,35 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QFont, QIcon, QPalette, QColor, QMovie
+from PyQt5.QtGui import QMovie
 import sys
 import os
 
-class Ui_MainWindow(QWidget):
-    def setupUi(self,MainWindow):
+
+class UIMainWindow(QWidget):
+    def __init__(self, main_window, *args, **kwargs):
         super().__init__()
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(400,200)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(0,0,400,200))
-        self.label.setMinimumSize(QtCore.QSize(400,200))
-        self.label.setMaximumSize(QtCore.QSize(400,200))
+        self.main_window = main_window
+        self.central_widget = QtWidgets.QWidget(self.main_window)
+        self.label = QtWidgets.QLabel(self.central_widget)
+        self.movie = QMovie(os.path.join("data", "Installing.gif"))
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.main_window.setObjectName("main_window")
+        self.main_window.resize(400, 200)
+        self.central_widget.setObjectName("central_widget")
+        self.label.setGeometry(QtCore.QRect(0, 0, 400, 200))
+        self.label.setMinimumSize(QtCore.QSize(400, 200))
+        self.label.setMaximumSize(QtCore.QSize(400, 200))
         self.label.setObjectName("label")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.movie = QMovie(os.path.join("data","Installing.gif"))
+        self.main_window.setCentralWidget(self.central_widget)
         self.label.setMovie(self.movie)
         self.movie.start()
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(window)
+    ui = UIMainWindow(window)
     window.show()
     sys.exit(app.exec_())
