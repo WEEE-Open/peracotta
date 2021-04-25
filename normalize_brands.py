da9bc386020800c03425583bbd65a9416947c37e
e-caste-mbp <en.cas.dev@icloud.com>
2021-04-25 12:10:20 +0200

    rename norm_script_alpha.py -> normalize_brands.py

--- normalize_brands.py
+++ normalize_brands.py
@@ -1,20 +1,21 @@
 from fuzzywuzzy import process
 
+
 def get_normalized(starting_name: str, names_coll: list):
     reading = process.extractBests(starting_name, names_coll, score_cutoff=75)
     candidates = [starting_name]
     best_n = starting_name
     max_n = len(reading)
 
-    while reading != []:
-        (new,points) = reading.pop()
+    while reading:
+        (new, points) = reading.pop()
         if points < 100 and new not in candidates:
             candidates.append(new)
             tmp = process.extractBests(new, li, score_cutoff=75)
             reading = reading + tmp
             if len(tmp) > max_n:
                 best_n = new
-    return (best_n, candidates)
+    return best_n, candidates
 
 
 def write_csv(out: dict):
