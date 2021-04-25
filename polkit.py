d4e73ad78f0a5ceb8915443b0336497d5b5dd79b
e-caste-mbp <en.cas.dev@icloud.com>
2021-04-25 11:55:59 +0200

    rename terminal_move.sh to more meaningful move_pkexec_policy_file.sh

--- polkit.py
+++ polkit.py
@@ -58,7 +58,7 @@ def make_dotfiles(path_to_generate_files_sh: str):
     if not os.path.isfile(path_to_dotpolicy):
         with open(local_path_to_dotpolicy, 'w') as f:
             f.write(dotpolicy_with_path)
-            os.system("x-terminal-emulator -e ./terminal_move.sh")
+            os.system("x-terminal-emulator -e ./move_pkexec_policy_file.sh")
             while not os.path.exists(path_to_dotpolicy):
                 sleep(0.1)
             print(path_to_dotpolicy, "was created!")
