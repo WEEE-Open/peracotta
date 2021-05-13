a6b38a703a1b424ac7cebf7c0d42ef31ce76ae45
e-caste-mbp <en.cas.dev@icloud.com>
2021-05-13 19:06:38 +0200

    try to fix output path in generate_files.sh. ref #119

--- scripts/generate_files.sh
+++ scripts/generate_files.sh
@@ -9,8 +9,9 @@ if [ $# -eq 0 ]; then
     echo "No path given: outputting files to working directory"
     OUTPATH="."
 elif [ $# -eq 1 ]; then
-    echo "Outputting files to "$1
-    OUTPATH=$1
+    echo "Outputting files to $1"
+    # if path is absolute, use it, otherwise enforce relative path for pkexec to prevent /root/tmp
+    [[ $1 =~ ^/ ]] && OUTPATH=$1 || OUTPATH="./$1"
 else
     echo -n "Unexpected number of parameters.\nUsage: sudo ./generate_files.sh /optional/path/to/files"
 fi
