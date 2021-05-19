05582cd1f1039aea71632fbaef49e9e5833e518f
e-caste-weeedev <en.cas.dev@icloud.com>
2021-05-19 22:16:40 +0200

    better bash syntax in generate_files.sh

--- scripts/generate_files.sh
+++ scripts/generate_files.sh
@@ -9,8 +9,8 @@ if [ $# -eq 0 ]; then
     echo "No path given: outputting files to working directory"
     OUTPATH="."
 elif [ $# -eq 1 ]; then
-    echo "Outputting files to "$1
-    OUTPATH=$1
+    echo "Outputting files to $1"
+    OUTPATH="$1"
 else
     echo -n "Unexpected number of parameters.\nUsage: sudo ./generate_files.sh /optional/path/to/files"
 fi
@@ -27,13 +27,12 @@ truncate -s 0 "$OUTPATH/net.txt"  # Create empty file or delete content
 NET=($(find /sys/class/net -maxdepth 1 \( -name "en*" -o -name "wl*" \) -exec basename  '{}' ';'))
 for NETDEV in "${NET[@]}"
 do
-	ADDRESS=$(cat /sys/class/net/$NETDEV/address)
-	if [[ $? -ne 0 ]]
-	then
+	ADDRESS=$(cat "/sys/class/net/$NETDEV/address")
+	if [[ $? -ne 0 ]]; then
 		echo The \"invalid argument\" error above is normal, disregard it
 	fi
-	SPEED=$(cat /sys/class/net/$NETDEV/speed)
-	echo $NETDEV $ADDRESS $SPEED >> "$OUTPATH/net.txt"
+	SPEED=$(cat "/sys/class/net/$NETDEV/speed")
+	echo "$NETDEV $ADDRESS $SPEED" >> "$OUTPATH/net.txt"
 done
 lscpu > "$OUTPATH/lscpu.txt"
 lspci -v > "$OUTPATH/lspci.txt"
@@ -41,11 +40,8 @@ glxinfo > "$OUTPATH/glxinfo.txt"
 DISKZ=($(lsblk -d -I 8 -o NAME -n))
 echo Found ${#DISKZ[@]} disks
 for d in "${DISKZ[@]}"; do
-	smartctl -x /dev/$d > "$OUTPATH/smartctl-dev-$d.txt"
+	smartctl -x /dev/"$d" > "$OUTPATH/smartctl-dev-$d.txt"
 done
 
-#Add an empty file if no disk is detected
-if ${#DISKZ[@]} -eq 0
-then
-touch "$OUTPATH/smartctl-dev-nodisk.txt"
-fi
+# Add an empty file if no disk is detected
+[[ ${#DISKZ[@]} -eq 0 ]] && touch "$OUTPATH/smartctl-dev-nodisk.txt"
