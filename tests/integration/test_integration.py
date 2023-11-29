4219fef0fdf0630983abdadbc2937e4bde905109
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2023-11-29 09:48:56 +0100

    removed old tests

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -20,7 +20,7 @@ def import_executable(name):
     return module
 
 
-peracruda = import_executable("peracruda")
+# peracruda = import_executable("src/peracruda.py")
 
 
 test_folders = [entries for entries in os.listdir("tests/source_files/") if os.path.isdir(f"tests/source_files/{entries}")]
@@ -76,52 +76,52 @@ def load_dotenv_for_upload():
     load_dotenv()
 
 
-@pytest.mark.upload
-def test_upload_pytarallo(args2, monkeypatch, capsys):  # testing pytarallo integration with peracotta
-    def auto_bulk_id():  # testing straight upload with automatic bulk_id
-        cli_input = io.StringIO("y\n\n")
-        monkeypatch.setattr("sys.stdin", cli_input)
-        peracruda.upload_to_tarallo(result)
-        assert "all went fine" in capsys.readouterr().out.lower()
+# @pytest.mark.upload
+# def test_upload_pytarallo(args2, monkeypatch, capsys):  # testing pytarallo integration with peracotta
+# def auto_bulk_id():  # testing straight upload with automatic bulk_id
+#    cli_input = io.StringIO("y\n\n")
+#    monkeypatch.setattr("sys.stdin", cli_input)
+#    peracruda.upload_to_tarallo(result)
+#    assert "all went fine" in capsys.readouterr().out.lower()
 
-    def fixed_bulk_id():  # testing automatic upload with a fixed bulk_id
-        identifier = f"{args2[0]}{time.time()}"
-        cli_input = io.StringIO(f"y\n{identifier}\n")
-        monkeypatch.setattr("sys.stdin", cli_input)
-        peracruda.upload_to_tarallo(result)
-        assert "all went fine" in capsys.readouterr().out.lower()
-        return identifier
+# def fixed_bulk_id():  # testing automatic upload with a fixed bulk_id
+#    identifier = f"{args2[0]}{time.time()}"
+#    cli_input = io.StringIO(f"y\n{identifier}\n")
+#    monkeypatch.setattr("sys.stdin", cli_input)
+#    peracruda.upload_to_tarallo(result)
+#    assert "all went fine" in capsys.readouterr().out.lower()
+#    return identifier
 
-    def overwrite_bulk_id(
-        identifier,
-    ):  # testing the overwrite of a bulk_id with an upload
-        cli_input = io.StringIO(f"y\n{identifier}\ny\n")
-        monkeypatch.setattr("sys.stdin", cli_input)
-        peracruda.upload_to_tarallo(result)
-        output = capsys.readouterr().out.lower()
-        assert "do you want to try overwriting" in output
-        assert "all went fine" in output
+# def overwrite_bulk_id(
+#    identifier,
+# ):  # testing the overwrite of a bulk_id with an upload
+#    cli_input = io.StringIO(f"y\n{identifier}\ny\n")
+#    monkeypatch.setattr("sys.stdin", cli_input)
+#    peracruda.upload_to_tarallo(result)
+#    output = capsys.readouterr().out.lower()
+#    assert "do you want to try overwriting" in output
+#    assert "all went fine" in output
 
-    def change_identifier(
-        old_identifier,
-    ):  # testing an upload with a new identifier after a failure
-        cli_input = io.StringIO(f"y\n{old_identifier}\nn\n{args2[0]}{time.time()}")
-        monkeypatch.setattr("sys.stdin", cli_input)
-        peracruda.upload_to_tarallo(result)
-        output = capsys.readouterr().out.lower()
-        assert "do you want to use another identifier" in output
-        assert "all went fine" in output
+# def change_identifier(
+#    old_identifier,
+# ):  # testing an upload with a new identifier after a failure
+#    cli_input = io.StringIO(f"y\n{old_identifier}\nn\n{args2[0]}{time.time()}")
+#    monkeypatch.setattr("sys.stdin", cli_input)
+#    peracruda.upload_to_tarallo(result)
+#    output = capsys.readouterr().out.lower()
+#    assert "do you want to use another identifier" in output
+#    assert "all went fine" in output
 
-    try:
-        result = commons.call_parsers(args2[0], args2[1], args2[2])
-        result = commons.split_products(result)
-        result = commons.make_tree(result)
+# try:
+#    result = commons.call_parsers(args2[0], args2[1], args2[2])
+#    result = commons.split_products(result)
+#    result = commons.make_tree(result)
 
-        auto_bulk_id()
-        overwrite_id = fixed_bulk_id()
-        overwrite_bulk_id(overwrite_id)
-        change_identifier(overwrite_id)
+#    auto_bulk_id()
+#    overwrite_id = fixed_bulk_id()
+#    overwrite_bulk_id(overwrite_id)
+#    change_identifier(overwrite_id)
 
-    except EnvironmentError:
-        # if this exception is raised probably bad config of peracotta/.env -> test must fail
-        assert False, "Bad peracotta .env configuration"
+# except EnvironmentError:
+#    # if this exception is raised probably bad config of peracotta/.env -> test must fail
+#    assert False, "Bad peracotta .env configuration"
