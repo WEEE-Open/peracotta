53860a2ba688973cbe4922e0fb7ce8c9715df33d
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2023-12-20 11:28:04 +0100

    moved logs to .config/peracotta

--- src/config.py
+++ src/config.py
@@ -8,18 +8,19 @@ from .commons import env_to_bool
 home_dir = Path().home()
 
 conf_dir = home_dir.joinpath(".config/peracotta")
+CONFIG = {}
 
 
 def load_conf():
+    global CONFIG
     load_dotenv(conf_dir.joinpath(".env"))
+    CONFIG = {
+        "TARALLO_URL": os.environ.get("TARALLO_URL", None),
+        "TARALLO_TOKEN": os.environ.get("TARALLO_TOKEN", None),
+        "TARALLO_FEATURES_AUTO_DOWNLOAD": env_to_bool(os.environ.get("TARALLO_FEATURES_AUTO_DOWNLOAD", "1")),
+        "GENERATE_FILES_USE_SUDO": env_to_bool(os.environ.get("GENERATE_FILES_USE_SUDO", "1")),
+        "GENERATE_FILES_ASK_SUDO_PASSWORD": env_to_bool(os.environ.get("GENERATE_FILES_ASK_SUDO_PASSWORD", "1")),
+    }
 
 
 load_conf()
-
-CONFIG = {
-    "TARALLO_URL": os.environ.get("TARALLO_URL", None),
-    "TARALLO_TOKEN": os.environ.get("TARALLO_TOKEN", None),
-    "TARALLO_FEATURES_AUTO_DOWNLOAD": env_to_bool(os.environ.get("TARALLO_FEATURES_AUTO_DOWNLOAD", "1")),
-    "GENERATE_FILES_USE_SUDO": env_to_bool(os.environ.get("GENERATE_FILES_USE_SUDO", "1")),
-    "GENERATE_FILES_ASK_SUDO_PASSWORD": env_to_bool(os.environ.get("GENERATE_FILES_ASK_SUDO_PASSWORD", "1")),
-}
