5d6a062c05a4809e69a4d3893a140998b0dd3e8b
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2023-12-23 19:11:29 +0100

    extended documentation and added log when erroring during crash report

--- src/__init__.py
+++ src/__init__.py
@@ -14,7 +14,31 @@ Some interesting properties are:
  - **[project.scripts]** define commands that will be installed in the system (usually in ~/.local/bin, make sure it's in PATH) and what functions in the package they should call.
  - **[project.gui-scripts]** same thing but for commands that start a GUI.
  - **[tool.setuptools.package-data]** by default, only .py files are included in the built package. This property allows including useful data files such as assets and scripts. Find more info [here](https://setuptools.pypa.io/en/latest/userguide/datafiles.html#package-data).
+
+## Styling
+as a general rule for styling try to follow [Google Python Style Guide](https://google.github.io/styleguide/pyguide.html). Formatting is done with black and isort (installed as a pre-commit hook).
+
+## Contributing
+You should **NEVER** push directly to the master branch.
+Work on each feature/improvement/bugfix in a separate branch (usually in a fork in your personal github profile).
+
+If two or more branches need to be merged together don't merge both in master, but first merge one into the other and resolve conflicts, then merge into master.
+
+Only ever merge into master if all tests are passing.
+
+When merging a branch, only use the 'rebase' option if there isn't more than a couple of commits. Otherwise, prefer 'squash'.
+
+Be coincise and descriptive with your commits.
+Avoid nonsensical or ambiguous descriptions, they will only make your life miserable when a new bug is found tomorrow and you have to trace back the root cause.
+
+If you modify existing code make sure you keep the documentation up-to-date.
+Mostly, the documentation is done through docstrings so it should be easy.
+The docstrings should ideally be updated in the same commit that modified the interested code.
+
+## Adding new features
+Whenever a new feature is added make sure that you have added proper tests and documentation before merging
 """
+
 import signal
 import sys
 
@@ -24,7 +48,7 @@ from . import peracruda
 from .commons import env_to_bool
 from .config import CONFIG
 from .constants import VERSION
-from .gui import GUI, gui_excepthook, errored
+from .gui import GUI, errored, gui_excepthook
 from .peralog import logdir, logger
 from .reporter import send_report
 
@@ -59,8 +83,9 @@ def main_gui():
     if CONFIG["AUTOMATIC_REPORT_ERRORS"] and errored():
         try:
             send_report()
-        except Exception:
-            pass
+        except Exception as e:
+            logger.info("Couldn't upload crash log.")
+            logger.exception(e)
 
 
 def main_cli():
