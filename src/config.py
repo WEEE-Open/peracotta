3d30b3fe71565f9e49f591ebf684396d00dc2897
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2024-03-15 16:11:40 +0100

    added documentation

--- src/config.py
+++ src/config.py
@@ -1,3 +1,24 @@
+"""
+Peracotta is configurable in various ways.
+The reccomended is with the file `~/.config/WEEE Open/peracotta/config.toml`.
+
+Example config:
+```toml
+TARALLO_URL = "http://192.168.2.142:8080"
+TARALLO_TOKEN = "yoLeCHmEhNNseN0BlG0s3A:ksfPYziGg7ebj0goT0Zc7pbmQEIYvZpRTIkwuscAM_k"
+TARALLO_FEATURES_AUTO_DOWNLOAD = false
+
+GENERATE_FILES_USE_SUDO = true
+GENERATE_FILES_ASK_SUDO_PASSWORD = true
+
+AUTOMATIC_REPORT_ERRORS = true
+REPORT_URL = "http://127.0.0.1:9999"
+```
+This configuration can be overridden by `~/.config/WEEE Open/peracotta/.env`
+and/or environment variables. If both are used, the latter take precedence.
+
+For compatibility with the old M.I.S.O.'s martello.sh script, it's also possible to configure it by placing a .env file in the src directory. For the same reasons, there's a .env.example in the source code at the appropriate place
+"""
 import os
 from pathlib import Path
 
@@ -8,7 +29,7 @@ from .commons import parse_from_env
 
 HOME_DIR = Path().home()
 
-CONF_DIR = HOME_DIR.joinpath(".config/peracotta")
+CONF_DIR = HOME_DIR.joinpath(".config/WEEE Open/peracotta")
 CONFIG = {}
 
 keys = [
@@ -24,6 +45,15 @@ keys = [
 # 1) local environment
 for key in keys:
     CONFIG[key] = parse_from_env(os.environ.get(key))
+
+# 1.1) src's .env, for compatibility with old M.I.S.O.
+try:
+    load_dotenv(".env")
+    for key in keys:
+        if key not in CONFIG.keys() or CONFIG[key] is None:
+            CONFIG[key] = parse_from_env(os.environ.get(key))
+except FileNotFoundError:
+    pass
 
 # 2) CONF_DIR's .env
 try:
